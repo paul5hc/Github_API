56d80dfa7b9ac26515c07ddfe625535016514102
paul5hc <byrnep21@tcd.ie>
2017-12-12 14:43:38 +0000

    Added function to retrieve repositories.

--- Github_API.js
+++ Github_API.js
@@ -12,20 +12,34 @@ github.users.getFollowingForUser({
   username: 'paul5hc'
 }, function (err, res) {
   if (err) throw err
-  var keys = Object.keys(res.data);
-  console.log("\nFollowing:")
+  //var keys = Object.keys(res.data);
+  console.log("\n*****Following:*****")
   for (x=0; x<res.data.length; x++){
     console.log(res.data[x].login)
   }
+  console.log("Number of Following: " + (res.data.length))
 })
 
 github.users.getFollowersForUser({
   username: 'paul5hc'
 }, function (err, res) {
   if (err) throw err
-  var keys = Object.keys(res.data);
-  console.log("\nFollowers:")
+  //var keys = Object.keys(res.data);
+  console.log("\n*****Followers*****")
   for (x=0; x<res.data.length; x++){
     console.log(res.data[x].login)
   }
+  console.log("Number of Followers: " + (res.data.length))
+})
+
+github.repos.getAll({
+  username: 'paul5hc'
+}, function (err, res) {
+  if (err) throw err
+  //var keys = Object.keys(res.data);
+  console.log("\n*****Repositories*****")
+  for (x=0; x<res.data.length; x++){
+    console.log(res.data[x].name)
+  }
+  console.log("Number of Repositories: " + (res.data.length))
 })
