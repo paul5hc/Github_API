d2300ba773e1275b5ead92985730bdf7f459c18e
paul5hc <byrnep21@tcd.ie>
2017-12-12 19:00:53 +0000

    3 Statistics Successfully Retrieved

--- Github_API.js
+++ Github_API.js
@@ -8,14 +8,15 @@ github.authenticate({
   token: 'X'
 })
 
+
+/******************************************************************************
 github.users.getFollowingForUser({
   username: 'paul5hc'
 }, function (err, res) {
   if (err) throw err
-  //var keys = Object.keys(res.data);
   console.log("\n*****Following:*****")
-  for (x=0; x<res.data.length; x++){
-    console.log(res.data[x].login)
+  /*for (x=0; x<res.data.length; x++){
+    console.log((x+1) + " " + (res.data[x].login))
   }
   console.log("Number of Following: " + (res.data.length))
 })
@@ -24,32 +25,74 @@ github.users.getFollowersForUser({
   username: 'paul5hc'
 }, function (err, res) {
   if (err) throw err
-  //var keys = Object.keys(res.data);
   console.log("\n*****Followers*****")
-  for (x=0; x<res.data.length; x++){
-    console.log(res.data[x].login)
+  /*for (x=0; x<res.data.length; x++){
+    console.log((x+1) + " " + (res.data[x].login))
   }
   console.log("Number of Followers: " + (res.data.length))
 })
+*******************************************************************************/
 
-github.repos.getAll({
+github.repos.getAll(
+{
   username: 'paul5hc'
 }, function (err, res) {
   if (err) throw err
-  //var keys = Object.keys(res.data);
-  console.log("\n*****Repositories*****")
+  var repoArray = new Array(res.data.length)
+  var sizeArray = new Array(res.data.length)
+  var commitsArray = new Array(res.data.length)
+  //console.log("\n********Repos********\n")
   for (x=0; x<res.data.length; x++){
-    console.log(res.data[x].name)
-  }
-  console.log("Number of Repositories: " + (res.data.length))
-})
+    console.log((res.data[x].name) + ": " + (res.data[x].size) + "kb")
+    repoArray[x] = res.data[x].name
+    sizeArray[x] = res.data[x].size
+    github.repos.getCommits({
+      owner: 'paul5hc',
+      repo: repoArray[x]
+    }, function (err, res1)
+    {
+        if (err) throw err
+        console.log("\n********COMMITS********\n")
+      //  var keys = Object.keys(res.data[0])
+      //  console.log("\n" + keys + "\n")
 
-github.repos.getCommits({
+        commitsArray[x] = res1.data.length
+        console.log("Number of Commits: " + commitsArray[x])
+    })
+  }
+  console.log("\nNumber of repos = " + (x))
+
+  })
+
+
+
+/*github.repos.getCommits({
   owner: 'paul5hc',
-  repo: 'Github_API'
+  repo:  'Github_API'
 }, function (err, res) {
     if (err) throw err
-    for (x=0; x<res.data.length; x++){
-      console.log(res.data[x].commit.message)
+    var keys = Object.keys(res.data);
+    console.log(res.data[0])
+    /*for (x=0; x<res.data.length; x++){
+      console.log((x+1) + " " + (res.data[x].commit.message))
     }
-})
+})*/
+
+/*************************************************************************************************************/
+
+/*github.repos.getAll({
+  username: 'paul5hc'
+}, function (err, res) {
+  if (err) throw err
+  var keys = Object.keys(res.data[0]);
+  //console.log("\n*****Repositories*****")
+  console.log(res.data[5])
+})*/
+
+/*github.gists.getCommits({
+  id: 25330735
+}, function (err, res) {
+if (err) throw err
+//var keys = Object.keys(res.data[0]);
+console.log(res)
+})*/
